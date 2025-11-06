453c8a704be43d13aa7d9b13baeb035807670e03
baekbsy1111-star <baekbsy1111@gmail.com>
2025-11-06 16:05:13 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 
 int main(void) {
-    FILE *fp = NULL;
+    FILE *fp;
+    char input[100];
     char c;
 
     fp = fopen("sample.txt", "r");
     if (fp == NULL) {
-        printf("파일을 못열음\n");
-        return 1;
+        printf("Failed to open file!\n");
+        return -1;
     }
 
+#if 0
     while ((c = fgetc(fp)) != EOF) {
         putchar(c);
     }
-
+#else
+    while (fgets(input, 100, fp) != NULL) {
+        printf("%s", input);
+    }
+#endif
     fclose(fp);
-
-    system("PAUSE");  
-    return 0;         
+    system("PAUSE");
+    return 0;
 }
